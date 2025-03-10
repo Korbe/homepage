725eb7a02aef565e5102aee02285fef0d8cd6d87
Korbitsch Lukas <korbitsch.external@infineon.com>
2025-03-10 14:40:35 +0100

    Added transistion to photos

--- resources/js/Pages/Public/Home/Partials/Photos.vue
+++ resources/js/Pages/Public/Home/Partials/Photos.vue
@@ -5,7 +5,7 @@
         v-for="(image, imageIndex) in images"
         :key="image.src"
         :class="[
-          'relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl dark:bg-zinc-800 ',
+          'relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl dark:bg-zinc-800 hover:rotate-0 transition-transform duration-300',
           rotations[imageIndex % rotations.length]
         ]"
       >
@@ -26,7 +26,7 @@ const images = [
   { src: 'https://images.unsplash.com/photo-1509198397868-475647b2a1e5?q=80&w=2747&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
   { src: 'https://images.pexels.com/photos/213727/pexels-photo-213727.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
   { src: 'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-  { src: 'https://images.unsplash.com/photo-1582578598774-a377d4b32223?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { src: 'https://images.pexels.com/photos/4963437/pexels-photo-4963437.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
   { src: 'https://images.pexels.com/photos/260352/pexels-photo-260352.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
 ];
 </script>
