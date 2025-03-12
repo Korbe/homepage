accbb42c1a9a8b675247c32ef5ad39af296d92ac
Korbitsch Lukas <korbitsch.external@infineon.com>
2025-03-12 14:56:43 +0100

    Add CTA to projects

--- resources/js/Pages/Public/Projects/Projects.vue
+++ resources/js/Pages/Public/Projects/Projects.vue
@@ -16,11 +16,35 @@
                 </ul>
             </div>
 
-            <!-- <div>
-                <p>
-                    Ich helfe ich dir, digitale Projekte zum Leben zu erwecken - von Websites bis hin zu individuellen Webanwendungen.
+            <div class="text-zinc-600 dark:dark:text-zinc-100 text-center mt-20">
+                <h2 class="text-2xl font-bold tracking-tight text-zinc-800 sm:text-3xl dark:text-zinc-100 my-3">
+                    Du willst auch dein Projekt hier sehn?
+                </h2>
+                <p class="text-lg">
+                    Ich helfe dir gerne dein digitales Projekt zum Leben zu erwecken.
                 </p>
-            </div> -->
+                <p class="text-lg">
+                    Von Websites bis hin zu individuellen Webanwendungen
+                </p>
+                <div class="flex justify-center items-center gap-5 py-5">
+                <div class="flex ">
+                    <a :href="emailHref"
+                        class="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500">
+                        <i
+                            class="text-2xl fa-solid fa-envelope h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500"></i>
+                        <span class="ml-4">{{ emailText }}</span>
+                    </a>
+                </div>
+                <div class="flex">
+                    <a :href="phoneHref"
+                        class="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500">
+                        <i
+                            class="text-2xl fa-solid fa-phone h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500"></i>
+                        <span class="ml-4">{{ phoneText }}</span>
+                    </a>
+                </div>
+            </div>
+            </div>
         </Container>
     </PublicLayout>
 </template>
@@ -30,6 +54,23 @@ import PublicLayout from "@/Layouts/Public/PublicLayout.vue";
 import ProjectCard from "./Partials/ProjectCard.vue";
 import Container from "@/Components/Container.vue";
 import { WrenchScrewdriverIcon } from "@heroicons/vue/24/outline";
+import { ref, onMounted } from "vue";
+
+const emailHref = ref("");
+const emailText = ref("");
+const phoneHref = ref("");
+const phoneText = ref("");
+
+onMounted(() => {
+    const user = "korbitschl";
+    const domain = "gmail.com";
+    emailHref.value = `mailto:${user}@${domain}`;
+    emailText.value = `${user}@${domain}`;
+
+    phoneHref.value = "tel:+436601401425";
+    phoneText.value = "+43 660 14 0 14 25";
+});
+
 
 const projects = [
     {
@@ -54,7 +95,7 @@ const projects = [
             { href: "https://korbitsch.at", label: "korbitsch.at" },
             { href: "https://github.com/Korbe/homepage", label: "GitHub" },
         ],
-        logo: "http://homepage.test/images/me.jpg", // Kein Logo hier
+        logo: "/images/me.jpg",
         icon: null,
         techs: [
             { name: "Laraval", icon: "fa-laravel", color: "text-red-500" },
