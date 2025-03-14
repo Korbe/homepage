741358d79693630b6a51fae1c4bd8b3d8376aad5
Lukas Korbitsch <korbitschl@gmail.com>
2025-03-14 20:12:49 +0100

    fixed warnings

--- resources/js/Layouts/Public/MobileNavigation.vue
+++ resources/js/Layouts/Public/MobileNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <Popover v-bind="props">
+  <Popover>
     <PopoverButton
       class="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 ring-1 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
       Menu
