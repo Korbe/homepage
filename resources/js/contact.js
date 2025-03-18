ad19992e4b2d626a49ac63d5fb51526e5e422ee3
Lukas Korbitsch <korbitschl@gmail.com>
2025-03-18 22:17:06 +0100

    changed email

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -3,7 +3,7 @@ export default {
   firstName: 'Lukas',
   lastName: 'Korbitsch',
   org: "IT Dienst",
-  email: 'korbitschl' + '@' + 'gmail' + '.' + 'com',
+  email: 'info' + '@' + 'korbitsch' + '.' + 'at',
   phone: '+43' + '660' + '14' + '0' + '14' + '25',
   phoneText: '+43' + ' 660' + ' 14' + ' 0' + ' 14' + ' 25',
   website: 'korbitsch.at'
