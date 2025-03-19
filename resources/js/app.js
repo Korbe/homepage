47bab183bfa0dc839d9af018ff538690cdebe6ee
Lukas Korbitsch <korbitschl@gmail.com>
2025-03-19 19:29:16 +0100

    no message

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@ import { createInertiaApp, Link } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy";
 
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+const appName = import.meta.env.VITE_APP_NAME || "Korbitsch";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
