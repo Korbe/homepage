63b8b7e2367d2101d9a4834e70294b9f4811bd4a
Lukas Korbitsch <korbitschl@gmail.com>
2026-01-29 14:38:40 +0100

    added logo to splash screen

--- resources/js/Components/SplashScreen.vue
+++ resources/js/Components/SplashScreen.vue
@@ -1,6 +1,7 @@
 <template>
     <transition name="fade">
         <div aria-hidden="true" v-if="show" class="fixed inset-0 flex items-center flex-col justify-center bg-zinc-100 dark:bg-zinc-900 z-50">
+            <img src="/images/logo.png" alt="Korbitsch Logo" class="w-auto h-28 mb-4" />
             <h1 class="text-3xl animate-pulse text-brand-text dark:text-brand-text-dark">Korbitsch.at</h1>
         </div>
     </transition>
